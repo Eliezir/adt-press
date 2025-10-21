198000e1f2145c3c3158cd302666506780fed2e6
Nic Pottier <nicpottier@gmail.com>
2025-10-21 07:42:18 -0700

    actually fix code in ruff call

--- adt_eval/text_extraction.py
+++ adt_eval/text_extraction.py
@@ -1,9 +1,9 @@
 """Text extraction evaluation implementation.
-    
+
 Some notes on the scoring of matches:
 - The LabelStudio text_type dataset sometimes had errors in the text_transcript that were later corrected. Thus, in this script, we correct some of these errors as well (for example, removing double spaces).
 - Mismatches are often introduced by mismatched directional quotations, e.g. ’,”,‘,“. These are replaced by non-directional quotations in both the LLM output and the Gold Standard.
-- The match strategy implemented is a greedy one. 
+- The match strategy implemented is a greedy one.
     - For each line in the LLM output, a list of labels is created (where each item in the list is the text type corresponding to one repetition of the line in the LLM output).
     - For each line in the Gold Standard transcript, we seek a match and, if the line matches, one text type item is 'used up' from the list.
 """
@@ -53,12 +53,11 @@ class TextExtractionEvaluator(BaseEvaluator):
         actual_type_by_text = {}
         for group in page_texts.groups:
             for text_item in group.texts:
-
                 # Some mild cleaning on the text content to match the Gold Standard
-                text_item.text=text_item.text.replace("’", "'")
-                text_item.text=text_item.text.replace("”", '"')
-                text_item.text=text_item.text.replace("‘", "'")
-                text_item.text=text_item.text.replace("“", '"')
+                text_item.text = text_item.text.replace("’", "'")
+                text_item.text = text_item.text.replace("”", '"')
+                text_item.text = text_item.text.replace("‘", "'")
+                text_item.text = text_item.text.replace("“", '"')
 
                 if text_item.text not in actual_type_by_text.keys():
                     # If text does not yet appear in dictionary, insert as a 1-item list
@@ -67,7 +66,6 @@ class TextExtractionEvaluator(BaseEvaluator):
                     # If it exists, add to list
                     actual_type_by_text[text_item.text].append(text_item.text_type.value)
 
-
         # Compare with truth annotations
         matches = []
         for tt in truth:
@@ -75,21 +73,21 @@ class TextExtractionEvaluator(BaseEvaluator):
             text_type = tt["value"]["taxonomy"][0][0]
 
             # Some mild cleaning on the text content to match the Gold Standard
-            text_content=text_content.replace("\/","/")
-            text_content=text_content.replace("\\n","\n")
-            text_content=text_content.replace("  "," ")
-            text_content=text_content.replace("’", "'")
-            text_content=text_content.replace("”", '"')
-            text_content=text_content.replace("‘", "'")
-            text_content=text_content.replace("“", '"')
-            text_content=text_content.replace("\xad", "")
+            text_content = text_content.replace("\/", "/")
+            text_content = text_content.replace("\\n", "\n")
+            text_content = text_content.replace("  ", " ")
+            text_content = text_content.replace("’", "'")
+            text_content = text_content.replace("”", '"')
+            text_content = text_content.replace("‘", "'")
+            text_content = text_content.replace("“", '"')
+            text_content = text_content.replace("\xad", "")
 
             # Implement match between ground truth TT and actual LLM result, greedily taking the first text type in the list
-            if (text_content in actual_type_by_text):
+            if text_content in actual_type_by_text:
                 actual_type = actual_type_by_text[text_content].pop(0)
 
                 # Remove key if list is empty
-                if (len(actual_type_by_text[text_content]) == 0):
+                if len(actual_type_by_text[text_content]) == 0:
                     del actual_type_by_text[text_content]
             else:
                 actual_type = None
